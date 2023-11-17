6c65d61277079ef210d2fae8cc718b26da64c357
Jambukesh <jkesh360@gmail.com>
2023-11-17 17:17:12 +0530

    day9

---
+++ day9/day9/LargeDataCollection.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day10
+{
+   
+              public class LargeDataCollection : IDisposable
+            {
+                private List<Object> list;
+                private bool disposedvalue = false;
+                public LargeDataCollection(params Object[] a)
+                {
+                    list = new List<Object>(a);
+
+                    Console.WriteLine("List befor adding\\removing elements " + list.Count);
+                    Console.WriteLine("Elements in the list");
+                    foreach (Object o in list)
+                    {
+                        Console.WriteLine(o);
+                    }
+                }
+                public void AddElement(Object e)
+                {
+                    if (!disposedvalue)
+                    {
+                        list.Add(e);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Cannot add element after Disposal");
+                    }
+                }
+
+                public void RemoveElement(Object e)
+                {
+                    if (!disposedvalue)
+                    {
+                        list.Remove(e);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Cannot add element after Disposal");
+                    }
+                }
+
+                public object AccessElement(int i)
+                {
+                    if (!disposedvalue && i >= 0 && i < list.Count)
+                    {
+                        return list[i];
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Index or element does not exist");
+                        return null;
+                    }
+                }
+
+                public void DisplayElement()
+                {
+                    foreach (var item in list)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    Console.WriteLine();
+                }
+
+                protected virtual void Dispose(bool disposing)
+                {
+                    if (!disposedvalue)
+                    {
+                        if (disposing)
+                        {
+                            list.Clear();
+                            list = null;
+                        }
+
+                        Console.WriteLine("Large data collection has been disposed");
+                        disposedvalue = true;
+                    }
+                }
+                public void Dispose()
+                {
+                    Dispose(true);
+                    GC.SuppressFinalize(this);
+                }
+
+                ~LargeDataCollection()
+                {
+                    Dispose(false);
+                }
+
+            }
+        }
+
